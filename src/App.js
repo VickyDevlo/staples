62740659f0795c2fc551ea907ec49cbf7bf11d8f
Pranav <pranavmagare2020@gmail.com>
2022-05-24 16:07:56 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import MenuList from './Components/Navbar/MenuList';
+import Navbar from './Components/Navbar/Navbar';
+ import Banner from './Components/Banner/Banner'
+import Section from './Components/Section/Section';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+       
+        <Navbar />
+        <MenuList/>
+         <Banner/>
+         <Section/>
+         <Footer/>
+         
     </div>
   );
 }
